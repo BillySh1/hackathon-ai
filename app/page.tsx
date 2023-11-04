04ee40a5abb4f310bad6b114fca58aeb4d393941
billy <Billy@mask.io>
2023-11-05 00:01:57 +0800

    chore: init

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,19 @@
 "use client";
-import FormLabel from "@/components/FormLabel";
-import InputPanel from "@/components/InputPanel";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center">
-      <FormLabel />
-      <InputPanel />
-      <Link href={"/sd"} className="next">
-        {"NEXT ->"}
+    <div className="flex min-h-screen flex-col items-center justify-center">
+      <div className="hero">
+      <img className="hero-logo" src="/logo.svg" alt="" />
+
+      <img className="hero-title" src="/title.svg" alt="" />
+
+      </div>
+      <Link href={"/chat"} className="next" style={{
+        opacity: 0,
+      }}>
+        {"ENTER ->"}
       </Link>
     </div>
   );
